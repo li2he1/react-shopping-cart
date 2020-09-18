5189ec1b642c3582e33bfae01b150c321a8133dc
li2he1 <peterli2he1@gmail.com>
2020-09-18 15:45:52 -0400

    part 1

--- src/App.js
+++ src/App.js
@@ -1,18 +1,11 @@
 import React from 'react';
 import './App.css';
+import Home from './components/Home';
 
 function App() {
   return (
     <div className="App">
-        <nav>
-          <h2>Shop</h2>
-          <ul>
-            <li><a href="#">Home</a></li>
-            <li><a href="#">About</a></li>
-            <li><a href="#">Cart</a></li>
-          </ul>
-        </nav>
-      
+      <Home/>
     </div>
   );
 }
